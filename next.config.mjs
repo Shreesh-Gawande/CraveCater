bee71bd17e475782a1a24adbd90a8eff17ac3880
Shreesh-Gawande <shreeshgawande10@gmail.com>
2024-07-20 17:13:55 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol:'https',
-                hostname:'ap-south-1.graphassets.com',
-                port:'',
-                pathname:'/**'
-            },
-            {
-                protocol:'https',
-                hostname:'img.clerk.com',
-                port:'',
-                pathname:'/**'
-            }
-        ]
-    }
-};
-
-export default nextConfig;
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'ap-south-1.graphassets.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'img.clerk.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
